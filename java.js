2588ade30305b94588b2dcee9ac59e2ebb86bbc0
MrBusiness87 <bbartula87@gmail.com>
2020-06-19 21:45:25 -0400

    06/19/2020-managed to consolidate my code further and save space on my javascript file, added a few more visual changes to finale project. FINAL PUSH

--- java.js
+++ java.js
@@ -7,9 +7,9 @@ timeDiv.append(timeDisplay);
 
 const saveButton = document.querySelectorAll("button");
 
-const timeList = ["9am", "10am", "11am", "NOON", "1pm", "2pm", "3pm", "4pm", "5pm"];
+const timeList = ["9am", "10am", "11am", "NOON", "1pm", "2pm", "3pm", "4pm", "5pm", "10pm"];
 
-const idTime = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+const idTime = ["9", "10", "11", "12", "13", "14", "15", "16", "17", "22"];
 
 for (let i = 0; i < timeList.length; i++) {
   const newRow = $("<div class='row time-block'>").attr("id", idTime[i]);
@@ -31,38 +31,48 @@ for (let i = 0; i < timeList.length; i++) {
 
 localStorageFunction();
 
+
 function localStorageFunction() {
-  $("textarea")[0].value = localStorage.getItem("textarea1");
-  $("textarea")[1].value = localStorage.getItem("textarea2");
-  $("textarea")[2].value = localStorage.getItem("textarea3");
-  $("textarea")[3].value = localStorage.getItem("textarea4");
-  $("textarea")[4].value = localStorage.getItem("textarea5");
-  $("textarea")[5].value = localStorage.getItem("textarea6");
-  $("textarea")[6].value = localStorage.getItem("textarea7");
-  $("textarea")[7].value = localStorage.getItem("textarea8");
-  $("textarea")[8].value = localStorage.getItem("textarea9");
+  for (let i = 0; i < timeList.length; i++) {
+    $('textarea')[i].value = localStorage.getItem('textarea' + i);
+  }
+  // $("textarea")[0].value = localStorage.getItem("textarea1");
+  // $("textarea")[1].value = localStorage.getItem("textarea2");
+  // $("textarea")[2].value = localStorage.getItem("textarea3");
+  // $("textarea")[3].value = localStorage.getItem("textarea4");
+  // $("textarea")[4].value = localStorage.getItem("textarea5");
+  // $("textarea")[5].value = localStorage.getItem("textarea6");
+  // $("textarea")[6].value = localStorage.getItem("textarea7");
+  // $("textarea")[7].value = localStorage.getItem("textarea8");
+  // $("textarea")[8].value = localStorage.getItem("textarea9");
 }
 
 $("button").on("click", function (event) {
   event.preventDefault();
-  const textArea1 = $("textarea")[0].value;
-  const textArea2 = $("textarea")[1].value;
-  const textArea3 = $("textarea")[2].value;
-  const textArea4 = $("textarea")[3].value;
-  const textArea5 = $("textarea")[4].value;
-  const textArea6 = $("textarea")[5].value;
-  const textArea7 = $("textarea")[6].value;
-  const textArea8 = $("textarea")[7].value;
-  const textArea9 = $("textarea")[8].value;
-  localStorage.setItem("textarea1", textArea1);
-  localStorage.setItem("textarea2", textArea2);
-  localStorage.setItem("textarea3", textArea3);
-  localStorage.setItem("textarea4", textArea4);
-  localStorage.setItem("textarea5", textArea5);
-  localStorage.setItem("textarea6", textArea6);
-  localStorage.setItem("textarea7", textArea7);
-  localStorage.setItem("textarea8", textArea8);
-  localStorage.setItem("textarea9", textArea9);
+  for (let i = 0; i < timeList.length; i++) {
+    localStorage.setItem('textarea' + i, $('textarea')[i].value);
+  }
+  for (let i = 0; i < timeList.length; i++) {
+    const textarea = $('textarea' + i, textArea);
+  }
+  // const textArea1 = $("textarea")[0].value;
+  // const textArea2 = $("textarea")[1].value;
+  // const textArea3 = $("textarea")[2].value;
+  // const textArea4 = $("textarea")[3].value;
+  // const textArea5 = $("textarea")[4].value;
+  // const textArea6 = $("textarea")[5].value;
+  // const textArea7 = $("textarea")[6].value;
+  // const textArea8 = $("textarea")[7].value;
+  // const textArea9 = $("textarea")[8].value;
+  // localStorage.setItem("textarea1", textArea1);
+  // localStorage.setItem("textarea2", textArea2);
+  // localStorage.setItem("textarea3", textArea3);
+  // localStorage.setItem("textarea4", textArea4);
+  // localStorage.setItem("textarea5", textArea5);
+  // localStorage.setItem("textarea6", textArea6);
+  // localStorage.setItem("textarea7", textArea7);
+  // localStorage.setItem("textarea8", textArea8);
+  // localStorage.setItem("textarea9", textArea9);
 });
 
 function hourUpdater() {
